e55284ad6fd16d73605e62456f94b144e50cd368
JGeven <j.geven@student.fontys.nl>
2022-10-10 20:12:16 +0200

    Deleted unnecessary classes
    deleted Webclientconfig
    deleted User CLasses

--- MyManga-Backend/src/main/java/com/example/mymangabackend/repository/UsersRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.mymangabackend.repository;
-
-import com.example.mymangabackend.model.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UsersRepository extends JpaRepository<Users, Long> {
-}
