05925cf48abd1f3f5311342237aaa2a2b9d1bd6b
JGeven <j.geven@student.fontys.nl>
2022-10-19 10:53:10 +0200

    updated test properties

--- MyManga-Backend/src/test/java/com/example/mymangabackend/MyMangaBackendApplicationTests.java
+++ MyManga-Backend/src/test/java/com/example/mymangabackend/MyMangaBackendApplicationTests.java
@@ -2,8 +2,11 @@ package com.example.mymangabackend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource("classpath:application.properties")
 class MyMangaBackendApplicationTests {
 
 	@Test
